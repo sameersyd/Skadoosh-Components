689d9c1d1e935bf47ef073e10d400264dbed9d68
Sameer Nawaz <sameer.nwaz@gmail.com>
2020-09-26 13:36:48 +0530

    Initial Commit

---
+++ Skadoosh/SkadooshApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkadooshApp.swift
+//  Skadoosh
+//
+//  Created by Sameer Nawaz on 26/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SkadooshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
